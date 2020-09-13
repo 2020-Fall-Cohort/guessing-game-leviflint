23d4ef3cdbd221b9b35aece35c382ffaa223c9f5
leviflint <tweekex@hotmail.com>
2020-09-13 17:26:15 -0400

    6th iteration

--- src/main/java/GuessingGameApp.java
+++ src/main/java/GuessingGameApp.java
@@ -4,23 +4,24 @@ public class GuessingGameApp {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Please guess a number between 1 - 10:");
+        System.out.println("Please guess a whole number between 1 - 10:");
         int guess = input.nextInt();
-        if (guess == 7) {
+        int randomNumber = (int) (Math.random() * 10);
+        if (guess == randomNumber) {
             System.out.println("You win!");
         } else if (guess == -1){
             System.exit(0);
         } else if (guess == 0) {
             System.out.println("Only choose a number between 1 - 10");
-        } else if (guess < 7){
+        } else if (guess < randomNumber){
             System.out.println("The correct number is higher! You have one more guess.");
-        } else if (guess > 7){
+        } else if (guess > randomNumber){
             System.out.println("The correct number is lower! You have one more guess.");
         } else {
             System.out.println("Incorrect, you have one more guess");
         }
         int guess2 = input.nextInt();
-        if (guess2 == 7) {
+        if (guess2 == randomNumber) {
             System.out.println("You win!");
         } else {
             System.out.println("You lose.. Sorry Charlie");
